e76f38b2ad01b224bc4bc8600016ad4c8595dfdb
Kshitij Kotecha <kshitijkotecha@Kshitijs-MacBook-Air.local>
2024-01-06 15:25:10 +0530

    firstReact

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,41 @@ const imageData = ()=>{
 
 function App() {
   // code here
+
+  let data = imageData()
+
+  let galleryStyle = {
+    textAlign:"center"
+  }
+
+  let styleMydiv = {
+    display:"grid",
+    gridTemplateColoumns:"repeat(2,1fr)",
+
+  }
+
+  return(
+  <>
+  <h1 style={(galleryStyle)}>Kalvium Gallery </h1>
+
+{/* 1st Method */}
+  {/* <div style={styleMydiv}>
+    <img src={data[0].img} width={"200px"}></img>
+    <img src={data[1].img} width={"200px"}></img>
+    <img src={data[2].img} width={"200px"}></img>
+    <img src={data[3].img} width={"200px"}></img>
+  </div> */}
+
+{/* 2nd Method  */}
+  <div>
+    {data.map((el) => (
+      <div key={el.id}>
+        <img src={el.img} width={"300px"} style={{marginBottom: "50px"}} />
+        </div>
+    ))}
+  </div>
+  </>)
 }
+
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -27,4 +27,30 @@ export default class AppClass extends Component{
   }
 
   // code here
+  render(){
+    this.data = this.imageData()
+    console.log(this.data)
+
+    let galleryStyle = {
+      textAlign:"center"
+    }
+  
+    let styleMydiv = {
+      display:"grid",
+      gridTemplateColoumns:"repeat(2,1fr)",
+  
+    }
+    return(
+      <>
+      <h1 style={(galleryStyle)}>Kalvium Gallery </h1>
+      <div style={styleMydiv}>
+    <img src={this.data[0].img} width={"200px"}></img>
+    <img src={this.data[1].img} width={"200px"}></img>
+    <img src={this.data[2].img} width={"200px"}></img>
+    <img src={this.data[3].img} width={"200px"}></img>
+  </div>
+      
+      </>
+    )
+  }
 }
